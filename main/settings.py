d65e681f16f96b12660c4f69f9e28ab67f60a50f
David Smith <dasmith2@gmail.com>
2020-04-23 12:16:32 -0400

    .

--- main/settings.py
+++ main/settings.py
@@ -69,19 +69,9 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_STRING_IF_INVALID = 'Invalid template variable: %s'
+
 WSGI_APPLICATION = 'main.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
