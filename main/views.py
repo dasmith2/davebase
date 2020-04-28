59a0f642e29d4e7398530dbbdc8ca4495cebd7a0
David Smith <dasmith2@gmail.com>
2020-04-28 14:47:57 -0400

    .

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def this_site_js(request):
-  return render(request, 'js/this_site.js')
+  return render(request, 'js/this_site.js', content_type='text/javascript')
