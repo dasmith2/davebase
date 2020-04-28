2c41b268d8c20bea80a2ea3ee16b64c9461188e3
David Smith <dasmith2@gmail.com>
2020-04-28 14:37:12 -0400

    .

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import urls as auth_urls
 from django.urls import include, path
 from djavError.urls import djaverror_urls
 
-from example.urls import example_urls
 from main.views import this_site_js
 
 
@@ -12,7 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(auth_urls)),
     path('js/this_site.js', this_site_js, name='this_site_js'),
-    path('', include(example_urls)),
     path('', include(djaverror_urls))
 ]
 
